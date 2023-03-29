7dfc7ec302febbe8235c17393ebe20d6b60f3f56
Gri-ffin <gameshunter359@gmail.com>
2023-03-29 18:16:03 +0000

    add first page

---
+++ src/lib/Welcome.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+	import { onMount } from 'svelte'
+	import { fly } from 'svelte/transition'
+	let visible: boolean = false
+
+	onMount(() => {
+		visible = true
+	})
+</script>
+
+<div
+	class="flex flex-col items-center justify-center h-screen tracking-widest text-center space-y-2"
+>
+	{#if visible}
+		<p transition:fly={{ x: 50, duration: 2000 }} class="text-2xl md:text-4xl">
+			Hello, I&apos;m <span class="text-[#ff4d5a]">Yassine</span>
+		</p>
+		<p transition:fly={{ x: -50, duration: 2000 }} class="text-2xl md:text-4xl">
+			I&apos;m a full stack web developer.
+		</p>
+		<button
+			transition:fly={{ y: 50, delay: 2000, duration: 2000 }}
+			class="outline p-2 outline-[#ff4d5a] text-[#ff4d5a]">View my projects</button
+		>
+	{/if}
+</div>
