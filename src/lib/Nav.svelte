8bba10de0b8f721453bf6235a2f3a030aa29ab0b
Gri-ffin <gameshunter359@gmail.com>
2023-03-30 18:02:45 +0000

    add component

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,10 @@
+<nav
+	class="fixed bg-[#282828f2] flex justify-center md:justify-end items-center md:h-14 border-b backdrop-filter z-50 shadow-lg border-black w-full text-sm h-10 md:text-base"
+>
+	<ul class="flex md:mr-12 md:gap-10 gap-5">
+		<li><a href="#welcome">Home</a></li>
+		<li><a href="#about">About</a></li>
+		<li>Projects</li>
+		<li>Contact</li>
+	</ul>
+</nav>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,12 @@
 <script lang="ts">
 	import About from '$lib/About.svelte'
+	import Nav from '$lib/Nav.svelte'
 	import Welcome from '../lib/Welcome.svelte'
 	import './styles.css'
 </script>
 
 <main class="bg-[#1a1a1a] text-[#fafafa]">
-	<nav
-		class="fixed bg-[#282828f2] flex justify-center md:justify-end items-center md:h-14 border-b backdrop-filter z-50 shadow-lg border-black w-full text-sm h-10 md:text-base"
-	>
-		<ul class="flex md:mr-12 md:gap-10 gap-5">
-			<li><a href="#welcome">Home</a></li>
-			<li><a href="#about">About</a></li>
-			<li>Projects</li>
-			<li>Contact</li>
-		</ul>
-	</nav>
+	<Nav />
 	<Welcome />
 	<About />
 </main>
