9f4fd6573133338f2207133880780bdded7cbbca
Gri-ffin <gameshunter359@gmail.com>
2023-04-08 00:59:27 +0000

    update nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,25 +1,15 @@
-<script lang="ts">
-	import { page } from '$app/stores'
-</script>
-
 <nav
 	class="fixed bg-[#282828f2] flex justify-center md:justify-end items-center md:h-14 border-b backdrop-filter z-50 shadow-lg border-black w-full text-sm h-10 md:text-base"
 >
 	<ul class="flex md:mr-12 md:gap-10 gap-5">
 		<li>
-			<a
-				href="#welcome"
-				class={`${$page.url.hash === '#welcome' || $page.url.hash === '' ? 'text-redrose' : ''}`}
-				>Home</a
-			>
+			<a href="#welcome" class="hover:text-redrose">Home</a>
 		</li>
 		<li>
-			<a href="#about" class={`${$page.url.hash === '#about' ? 'text-redrose' : ''}`}>About</a>
+			<a href="#about" class="hover:text-redrose">About</a>
 		</li>
 		<li>
-			<a href="#projects" class={`${$page.url.hash === '#projects' ? 'text-redrose' : ''}`}
-				>Projects</a
-			>
+			<a href="#projects" class="hover:text-redrose">Projects</a>
 		</li>
 	</ul>
 </nav>
